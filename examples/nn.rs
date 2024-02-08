fef85996a99eaec06d2fbcb2d4fffffdab80778c
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2024-02-08 12:01:08 +0100

    optimize the shit out of it

--- examples/nn.rs
+++ examples/nn.rs
@@ -1,6 +1,10 @@
+use std::mem;
+use std::time::Instant;
 use rusty_brain::*;
 
 fn main() {
+    let start_time = Instant::now();
+
     // test data
     let mut td_in = Matrix::new(4, 2);
     td_in.content[0][0] = 0.;
@@ -26,22 +30,30 @@ fn main() {
 
     NeuralNetwork::print_nn(nn.clone());
     println!("------------------------------------------------");
-    println!("{}", NeuralNetwork::nn_cost(nn.clone(), td_in.clone(), td_out.clone()));
+    println!("{}", NeuralNetwork::nn_cost(&mut nn.clone(), &td_in, &td_out));
 
     for _i in 0..20*1000 {
-        (nn, nn_g) = NeuralNetwork::nn_fin_diff(nn, nn_g, 1e-1, td_in.clone(), td_out.clone());
+        NeuralNetwork::nn_fin_diff(&mut nn, &mut nn_g, 1e-1, &td_in, &td_out);
         nn = NeuralNetwork::nn_learn(nn, nn_g.clone(), 1e-1);
+
+        // uncomment for live progress
+        //println!("{}", NeuralNetwork::nn_cost(&mut nn, &td_in, &td_out));
     }
-    println!("{}", NeuralNetwork::nn_cost(nn.clone(), td_in.clone(), td_out.clone()));
+    println!("{}", NeuralNetwork::nn_cost(&mut nn, &td_in, &td_out));
 
     for i in 0..2 {
         for j in 0..2 {
             nn.a_s[0].content[0][0] = i as f32;
             nn.a_s[0].content[0][1] = j as f32;
 
-            nn = nn.nn_forward();
+            nn.nn_forward();
+
             let y = nn.a_s[nn.clone().nn_get_size() - 1].content[0][0];
             println!("{} ^ {} = {}", i, j, y)
         }
     }
+    let end_time = Instant::now();
+
+    let total_time = end_time - start_time;
+    println!("Time taken: {} seconds {} milliseconds", total_time.as_secs(), total_time.subsec_millis());
 }
